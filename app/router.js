a432e33236428188afadc8c2d90a805b0f3f3e88
dongyuanwai <yuanwaidong@gmail.com>
2022-11-19 16:26:18 +0800

    feat: 注册接口

--- app/router.js
+++ app/router.js
@@ -5,14 +5,6 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.get('/', controller.home.index);
-  router.get('/user/:id', controller.home.user);
-  router.get('/user', controller.home.user);
-
-  // post请求 添加
-  router.post('/add_user', controller.home.addUser);
-  // 编辑
-  router.post('/edit_user', controller.home.editUser);
-  // 删除
-  router.post('/delete_user', controller.home.deleteUser);
+  // 注册接口
+  router.post('/api/user/register', controller.user.register);
 };
